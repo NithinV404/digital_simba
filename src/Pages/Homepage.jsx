9945b0189dc9e9b2659e5e9ccfecf027816737bf
Nithin <nithin.v@truewit.in>
2025-07-23 17:37:46 +0530

    Removed extra import

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -1,7 +1,6 @@
 import Header from "../Components/Header";
 import Banner from "../Components/Homepage/banner";
 import AboutSection from "../Components/Homepage/about_us"
-import Banner from "../Components/Homepage/banner"
 import FeatureSection from "../Components/Homepage/features"
 
 export default function Homepage() {
