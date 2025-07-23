0fdcec5ed706f63073f8ea58d55f9fa9f267aaed
Nithin <nithin.v@truewit.in>
2025-07-23 16:51:35 +0530

    Added about us section to homepage

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -1,9 +1,11 @@
+import AboutSection from "../Components/Homepage/about_us"
 import Banner from "../Components/Homepage/banner"
 
 export default function Homepage() {
   return (
     <>
       <Banner />
+      <AboutSection /> 
     </>
   )
 }
