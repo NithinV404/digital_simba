1c96069507a07e5daa7bbdb4d70105669c9d0222
sandeep-hegde <sandeepa.s@truewit.in>
2025-07-24 12:47:43 +0530

    added powerful feature, to faq

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -1,6 +1,10 @@
 import Banner from "../Components/Homepage/banner";
 import AboutSection from "../Components/Homepage/about_us"
 import FeatureSection from "../Components/Homepage/features"
+import PowerfulFeature from "../Components/Homepage/powerful_feature";
+import Templates from "../Components/Homepage/templates";
+import WhySimba from "../Components/Homepage/whysimba";
+import FAQs from "../Components/Homepage/faqs";
 
 export default function Homepage() {
   return (
@@ -9,6 +13,10 @@ export default function Homepage() {
       <Banner />
       <AboutSection /> 
       <FeatureSection />
+      <PowerfulFeature />
+      <Templates /> 
+      <WhySimba />
+      <FAQs />
     </>
   );
 }
